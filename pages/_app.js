0ae1b0b3e9643bc6c756faa8fd1dacce352361c0
vectorCodes <ansarihassan0703@gmail.com>
2021-12-20 10:55:11 +0530

    added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/globals.css";
+import Router from "next/router";
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
+import "../styles/nprogress.css";
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+
+Router.events.on("routeChangeStart", nprogress.start);
+Router.events.on("routeChangeError", nprogress.done);
+Router.events.on("routeChangeComplete", nprogress.done);
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
